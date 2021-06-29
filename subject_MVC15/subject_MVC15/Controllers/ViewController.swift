076dedfc364c6b1eb9c6dbf53cb67fe81e76aa1d
Hase401 <83523126+Hase401@users.noreply.github.com>
2021-06-29 22:04:59 +0900

    Add files via upload

---
+++ subject_MVC15/subject_MVC15/Controllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  subject_MVC15
+//
+//  Created by 長谷川孝太 on 2021/06/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
